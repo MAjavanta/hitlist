0cd6c5cd5d52e70adc0df7048ba5023e3df3ff92
MAjavanta <ma.javanta@gmail.com>
2026-08-13 23:28:19 +0100

    write initial steam request to find game titles and tags

--- src/hitlist/__init__.py
+++ src/hitlist/__init__.py
@@ -1,2 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def has_class_search_result_row(tag):
+    return tag.has_attr("class") and "search_result_row" in tag["class"]
+
+
 def main() -> None:
-    print("Hello from hitlist!")
+    x = requests.get("https://store.steampowered.com/search?hwtype=0&category1=998")
+    soup = BeautifulSoup(x.text, "html.parser")
+
+    for row in soup.find_all("a", class_="search_result_row"):
+        appid = row["data-ds-appid"]
+        tags = row["data-ds-tagids"]
+        title = row.find("span", class_="title").get_text(strip=True)
+        print(title, appid, tags)
